8b1965d77f9af5734f14974dcb82640dea1f5f25
ashton davis <ajd96@proton.me>
2025-10-13 10:30:07 -0500

    added slurm parallelization to shaum's code

--- aricarte/run_ipole.sh
+++ aricarte/run_ipole.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+# make sure GSL libraries are available
 export LD_LIBRARY_PATH=/work/vmo703/aricarte/gsl-1.16/lib:/work/vmo703/aricarte/gsl-1.16/lib64:$LD_LIBRARY_PATH
-exec /work/vmo703/aricarte/aricarte-copy/aricarte/ipole+e-/ipole "$@"
+
+# set OpenMP threads to what Slurm gave us
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+echo "[$(date)] starting ipole with $OMP_NUM_THREADS threads"
+
+# run ipole
+/work/vmo703/aricarte/aricarte-copy/aricarte/ipole+e-/ipole "$@"
+
+echo "[$(date)] finished ipole"
